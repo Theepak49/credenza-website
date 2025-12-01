ba2a3babdc4eb24da9c7142c94a23cfeed904caa
innsof <theepak@innsof.com>
2025-12-02 01:47:35 +0530

    Changes 3

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
   },
-};
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  devIndicators: false
+}
 
-export default nextConfig;
+export default nextConfig
